83990f0b18e2192f6db1ee1fc75652481145af66
popcoondev <popcoondev@gmail.com>
2025-03-26 01:25:36 +0900

    Babel設定の改善
    - React/JSX変換用のプリセットを追加
    - テスト環境用の設定を追加
    - NODE_ENV=test環境変数を追加

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,15 @@
 module.exports = function(api) {
+  // testでアクセスするため、これをプロセス環境に応じて設定
+  const isTest = process.env.NODE_ENV === 'test';
   api.cache(true);
+  
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      'babel-preset-expo',
+      ['@babel/preset-env', { targets: { node: 'current' } }],
+      '@babel/preset-typescript',
+      ['@babel/preset-react', { runtime: 'automatic' }],
+    ],
     plugins: [
       ["module:react-native-dotenv", {
         "envName": "APP_ENV",
@@ -12,7 +20,9 @@ module.exports = function(api) {
         "safe": false,
         "allowUndefined": true,
         "verbose": false
-      }]
-    ]
+      }],
+      // テスト用の変換を追加
+      isTest && '@babel/plugin-transform-modules-commonjs',
+    ].filter(Boolean)
   };
 };
